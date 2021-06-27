547d5f7cef04f91fe97650b2f79211a993b5df28
Surin Lee <80635378+Elineely@users.noreply.github.com>
2021-06-27 18:53:51 +0900

    higher order function practice
    created a calculator function

---
+++ Higher_Oder_Function_practice.js
@@ -0,0 +1,23 @@
+function add (num1, num2){
+  return num1 + num2 ;
+}
+
+function multifly (num1, num2){
+  return num1 * num2 ;
+}
+
+function substrack (num1, num2){
+  return num1 - num2 ;
+}
+
+function devide (num1, num2){
+  return num1 / num2 ;
+}
+
+function modulo (num1, num2){
+  return num1 % num2 ;
+}
+
+function calculator(num1, num2, operator){
+  return operator(num1, num2);
+}
