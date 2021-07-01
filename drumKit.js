99fbf151fbc1a040faf2657ff6762b874a622e54
Surin Lee <80635378+Elineely@users.noreply.github.com>
2021-07-01 12:29:31 +0900

    drrmKit project
    made the each sounds(matted with the background picture) palyed when you clicked each the buttons.

--- drumKit.js
+++ drumKit.js
@@ -4,8 +4,42 @@ var buttonsDrum = document.querySelectorAll("button.drum");
 var numberOfButtons = document.querySelectorAll("button.drum").length;
 
 for(i = 0; i < numberOfButtons; i++){
-  var audio = new Audio("./soundsDK/tom-1.mp3");
   buttonsDrum[i].addEventListener("click", function ( ) {
-    audio.play( );
+  //this.setAttribute("style", "color : white;");
+  var bottonsInnerHTML = this.innerHTML;
+
+  switch (bottonsInnerHTML) {
+    case "w":
+      var tom1 = new Audio("./soundsDK/tom-1.mp3");
+      tom1.play( );
+      break;
+    case "a":
+      var tom2 = new Audio("./soundsDK/tom-2.mp3");
+      tom2.play( );
+      break;
+    case "s":
+      var tom3 = new Audio("./soundsDK/tom-3.mp3");
+      tom3.play( );
+      break;
+    case "d":
+      var tom4 = new Audio("./soundsDK/tom-4.mp3");
+      tom4.play( );
+      break;
+    case "j":
+      var crash = new Audio("./soundsDK/crash.mp3");
+      crash.play( );
+      break;
+    case "k":
+      var kickBass = new Audio("./soundsDK/kick-bass.mp3");
+      kickBass.play( );
+      break;
+    case "l":
+      var snare = new Audio("./soundsDK/snare.mp3");
+      snare.play( );
+      break;
+    default:
+
+  }
+
   });
 }
