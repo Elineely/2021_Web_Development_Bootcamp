fe7c0bb9f3d322726990b874aceef947b3eb8a0b
Surin Lee <80635378+Elineely@users.noreply.github.com>
2021-07-09 00:41:58 +0900

    simon game project
    - still making the game work

--- game.js
+++ game.js
@@ -1,10 +1,24 @@
+//variables
+
 var gamePattern = [ ];
+
+var level = 0;
+
+var started = false;
 
 var buttonColors = ["red", "blue", "green", "yellow"];
 
+var userClickedPattern = [ ];
+
+//functions
 function nextSequence(){
   var randomNumber = Math.floor(Math.random() * 4);
-  return randomNumber;
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  playAudio1(randomChosenColor);
+  $("#" + randomChosenColor).fadeOut(150).fadeIn(150).fadeOut(150).fadeIn(150);
+  level++;
+  return gamePattern, level;
 }
 
 function playAudio1(color){
@@ -28,16 +42,53 @@ function playAudio1(color){
   }
 }
 
-var randomChosenColor = buttonColors[nextSequence()];
-gamePattern.push(randomChosenColor);
-playAudio1(randomChosenColor);
-$("#" + randomChosenColor).fadeOut(150).fadeIn(150).fadeOut(150).fadeIn(150);
+function animatePress(currerntColor){
+  $("#" + currerntColor).toggleClass("pressed");
+  setTimeout(() => $("#" + currerntColor).toggleClass("pressed"), 100);
+}
 
-var userClickedPattern = [ ];
+function checkAnswer(currentLevel){
+  for(i=0 ; i <= currentLevel; i++){
+    if(gamePattern[i] === userClickedPattern[i]){
+      console.log("success");
+      return 1;
+    }else{
+      console.log("wrong");
+      return 0;
+    }
+  }
+}
 
-$(".btn").click( function( ) {
-  var chosenByUser = this.className.slice(4);
+function usersTurn() {
+  $(".btn").click( function( ) {
+  var chosenByUser = this.id;
   playAudio1(chosenByUser);
+  animatePress(chosenByUser);
   userClickedPattern.push(chosenByUser);
   return userClickedPattern;
+  });
+}
+
+
+//game start
+$(document).keypress( function(){
+  if(!started){
+    $("#level-title").text("Level. " + level);
+    setTimeout(() =>nextSequence(), 700);
+    started = true;
+
+    usersTurn();
+    if(userClickedPattern[0]){
+      if(level = 0){checkAnswer(0)
+      nextSequence()
+      usersTurn()}
+      while(level!=0 && checkAnswer(level)){
+        nextSequence();
+        usersTurn();
+        checkAnswer(level);
+      }
+
+    }
+
+  }
 });
