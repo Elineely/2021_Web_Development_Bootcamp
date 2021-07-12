339dcbf2631c5c8a87e6b9bda2583729502ae920
Surin Lee <80635378+Elineely@users.noreply.github.com>
2021-07-12 22:51:53 +0900

    Express.js with Node.js
    - Handling Requests and Reponses : the GET request

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require("express");
 var app = express();
 
+app.get("/", function(request, response){
+  response.send("<h1>Hello, there!</h1>");
+  console.log(request);
+});
+
 app.listen(3000, function(){
   console.log("Server started on port 3000");
 });
