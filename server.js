efbb49ab1459fb1a34e516b3d8a2f52a3e5d23fa
Surin Lee <80635378+Elineely@users.noreply.github.com>
2021-07-13 00:06:09 +0900

    Express.js with Node.js
    - added more routes
    - installed Nodemon from npm

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ app.get("/", function(request, response){
   console.log(request);
 });
 
+app.get("/contact", function(request, response){
+  response.send("<h2>Contact Me : fakemail.gmail.com </h2>");
+});
+
+app.get("/about", function(requset, response){
+  response.send("<h1>Surin Lee</h1> </br> <i>a baby developer 👶🏻 👩🏻‍💻 </i>");
+});
+
+app.get("/hobby", function(requset, response){
+  response.send("<h3> I Do love taking a walk with my doggie little sister.</h3>");
+});
+
 app.listen(3000, function(){
   console.log("Server started on port 3000");
 });
