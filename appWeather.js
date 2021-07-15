d73efb1cf9c24777e952d20cfb37419592de3a48
Surin Lee <80635378+Elineely@users.noreply.github.com>
2021-07-15 23:59:10 +0900

    Weather Project
    - Using the API

---
+++ appWeather.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const https = require("https");
+
+const app = express();
+
+app.get("/", function(req, res){
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=abaf6b7affaaf80dc46cf5afd8ac3545";
+  https.get(url, function(response){
+    console.log(response);
+  });
+  res.send("Server is up and running");
+});
+
+
+app.listen(3000, function(){
+  console.log("Server is running on port 3000");
+});
