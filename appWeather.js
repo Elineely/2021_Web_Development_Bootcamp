66bb512c4e206ecf08ebe1278bcd4b8248a8da75
Surin Lee <80635378+Elineely@users.noreply.github.com>
2021-07-17 16:30:46 +0900

    Weather Project
    - got dynamic data based on what the user typed into the input, 
    - caught that data in my app.post,
    - used that query to structure my URL and get the data for that particular location.

--- appWeather.js
+++ appWeather.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended : true}));
 
 
 app.get("/", function(req, res){
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=Seoul&units=metric&appid=abaf6b7affaaf80dc46cf5afd8ac3545";
+  res.sendfile(__dirname + "/indexWeather.html");
+
+});
+
+app.post("/", function(req, res){
+  var query = req.body.cityName;
+  const appid = "abaf6b7affaaf80dc46cf5afd8ac3545";
+  const units = "metric";
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&units="+ units +"&appid="+ appid ;
 
   https.get(url, function(response){
     console.log(response.statusCode);
@@ -20,25 +32,17 @@ app.get("/", function(req, res){
       const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
       console.log(temp);
       console.log(description);
-      res.write("<h1>The temperature in Sueoul is " + temp + " degrees Celcius now.</h1>");
+      res.write("<h1>The temperature in "+ query +" is " + temp + " degrees Celcius now.</h1>");
       res.write("<p>The weather is currently " + description +".</p>");
       res.write("<img src = "+imageURL+">");
       res.send();
     });
   });
-  //res.sendfile(__dirname + "/indexWeather.html");
 });
+
 
 
 
 app.listen(3000, function(){
   console.log("Server is running on port 3000");
 });
-
-//https.get("https://encrypted.google.com/", (response)=>{
-//  console.log("statusCode: ", response.statusCode);
-//  console.log("headers: ", response.headers);
-//});
-
-//    response.on("data", function(data){
-//      console.log(data);
